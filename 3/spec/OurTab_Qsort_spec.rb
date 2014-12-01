aa16d433be09e65ecd6fe1823f0b66ac6160bc23
Whetold <lmielnikowski@sigma.ug.edu.pl>
2014-12-01 20:05:52 +0100

    Update OurTab_Qsort_spec.rb
    wrrr

--- 3/spec/OurTab_Qsort_spec.rb
+++ 3/spec/OurTab_Qsort_spec.rb
@@ -4,37 +4,37 @@ require_relative '../lib/OurTab.rb'
 require_relative '../lib/Qsort.rb'
 
 SimpleCov.start
-
+#
 describe Qsort do
-	let(:cla){Class.new {include Qsort}}
+ let(:cla){Class.new {include Qsort}}
 
-	describe "#deletethat" do
-		it "Should deleted given number from array" do
-			expect(OurTab.new().setS([5,6,7,8]).deletethat(6)).to eq([5,7,8])
-		end
-	end
+ describe "#deletethat" do
+  it "Should deleted given number from array" do
+   expect(OurTab.new().setS([5,6,7,8]).deletethat(6)).to eq([5,7,8])
+  end
+ end
 
-	describe "#increaseit" do
-		let(:increase) { OurTab.new().setS([5,6,7,8]).increaseit(2)} 
+ describe "#increaseit" do
+  let(:increase) { OurTab.new().setS([5,6,7,8]).increaseit(2)} 
 		
-		it "Should increase whole array by number given" do
-			expect(increase).to eq([7,8,9,10])
-		end
+  it "Should increase whole array by number given" do
+   expect(increase).to eq([7,8,9,10])
+  end
 		
-	end
-	describe "#decreaseit" do
-	let(:decrease) { OurTab.new().setS([5,6,7,8]).decreaseit(2)} 
-	it "Should decrease the whole array by number given" do
-			expect(decrease).to eq([3,4,5,6])
-		end
-	end	
+ end
+ describe "#decreaseit" do
+ let(:decrease) { OurTab.new().setS([5,6,7,8]).decreaseit(2)} 
+ it "Should decrease the whole array by number given" do
+   expect(decrease).to eq([3,4,5,6])
+  end
+ end	
 
-	describe "#quicksort" do
-	let(:magic) { a = OurTab.new().setS([10,5,2,1])
-								a.quicksort(a,0,3)
-							}
-		it "Should put numbers in increasing order" do
-			expect(magic).to eq([1,2,5,10])
-		end
-	end
+ describe "#quicksort" do
+ let(:magic) { a = OurTab.new().setS([10,5,2,1])
+               a.quicksort(a,0,3)
+             }
+  it "Should put numbers in increasing order" do
+   expect(magic).to eq([1,2,5,10])
+  end
+end
 end
