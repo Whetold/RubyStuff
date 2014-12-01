9a827ad58c530db418f0c3f6715adce234cc6eb4
Whetold <whetold@hotmail.com>
2014-12-01 18:17:49 +0100

    Fixes

---
+++ 3/spec/OurTab_Qsort_spec.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+require 'rspec/mocks/standalone'
+require_relative '../lib/OurTab.rb'
+require_relative '../lib/Qsort.rb'
+
+SimpleCov.start
+
+describe Qsort do
+	let(:cla){Class.new {include Qsort}}
+
+	describe "#reset" do
+
+
+	end
+	
+
+end
